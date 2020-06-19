9196edf58125df6242a0d9bd354d0a7c359a4f89
Vilnei Neves <vilnei.neves@inf.ufpel.edu.br>
2020-06-19 11:00:02 -0300

    Add vagrant snapshot to run.sh

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -10,5 +10,11 @@ vagrant up
 # copy vagrant-ssh config to solve references in ansible playbook
 vagrant ssh-config > ~/.ssh/config
 
-# run ansible playbook
+#  create snapshot ubuntu  clean
+#vagrant snapshot save ubuntu-clean
+
+# run ansible playbook - baremetal
 ansible-playbook -i playbooks/openstack/hosts playbooks/openstack/playbook.yml 
+
+# run ansible playbook - vagrant
+#ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory playbooks/openstack/playbook.yml 
