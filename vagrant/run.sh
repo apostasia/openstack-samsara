b402248d9503c154d94edaad25ec48da9cf52129
marilia pit <mpdsantos@inf.ufpel.edu.br>
2020-07-05 00:50:49 +0000

    update: replaces samsara-rootwrap, using systemd for host hibernation

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -23,7 +23,7 @@ vagrant snapshot save openstack-clean
 #ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory playbooks/openstack/playbook.yml 
 
 # run ansible playbook - samsara
-ansible-playbook -i playbooks/openstack/hosts playbooks/samsara/playbook.yml
+#ansible-playbook -i playbooks/samsara/hosts playbooks/samsara/playbook.yml
 
 # Create snapshot Samsara clean.
-vagrant snapshot save samsara-clean
+#vagrant snapshot save samsara-clean
